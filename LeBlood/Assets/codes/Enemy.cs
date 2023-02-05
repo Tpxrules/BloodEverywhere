a0a4554933cbe5ed9f90e4e6cc3036cbf08224bd
Tpxrules <tpxrules@gmail.com>
2023-02-05 12:33:36 +0300

    enemy follow
    added two types of following to the enemy code that you can change with a check , if it is a ranger it will follow you and stop from a certain distance , if not then it will follow you until it is stuck to you

--- LeBlood/Assets/codes/Enemy.cs
+++ LeBlood/Assets/codes/Enemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject player;
+    public float speed;
+    private float distance;
+    public bool ranger;
     [SerializeField] float health, maxHealth = 3f;
 
     private void Start(){
@@ -15,5 +19,21 @@ public class Enemy : MonoBehaviour
         if(health<=0){
             Destroy(gameObject);
         }
+    }
+
+     public void Update() {
+        distance = Vector2.Distance(transform.position,player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+         
+       
+         if(ranger){
+
+                if(distance>4)
+                     transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+                
+
+         }else
+                    transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+         
     }
 }
