2bc101ba67888d1310a32e0663d09bed2afcfb0a
Tpxrules <tpxrules@gmail.com>
2023-02-14 19:38:21 +0300

    spawning power ups and cleaned some code

--- LeBlood/Assets/MangetCode.cs
+++ LeBlood/Assets/MangetCode.cs
@@ -7,16 +7,33 @@ public class MangetCode : MonoBehaviour
    
       public float duration = 4f;
 
+
+
+
     public IEnumerator Pickup(movement stats)
 {
     Debug.Log("Starting Invincibility Pickup");
+
+
+
+
+
     // Make the player invincible
     stats.magnet = true;
 
+
+
     yield return new WaitForSeconds(duration);
+
+
+
 
     // Reset the player's invincibility
     stats.magnet = false;
 }
+
+
+
+
 
 }
--- LeBlood/Assets/codes/lettherebeenemies.cs
+++ LeBlood/Assets/codes/lettherebeenemies.cs
@@ -4,43 +4,30 @@ using UnityEngine;
 
 public class lettherebeenemies : MonoBehaviour
 { 
+     public int size;
      public float spawntime = 0.1f; 
      private float timesincelastspawn;
-          public GameObject teddy1;
-           public GameObject teddy2;
-            public GameObject slime;
-        public GameObject b;
+
+             public GameObject[] c;
+              
     // Update is called once per frame
     void Update()
     {
 
+       
        timesincelastspawn += Time.deltaTime;
         if (timesincelastspawn >= spawntime )
         { 
           timesincelastspawn = 0f;
 
-
-          switch(Random.Range(0,6)){
-               case 1:
-                Instantiate(teddy1, new Vector2(Random.Range(-10,10),Random.Range(-10,10)), Quaternion.identity);
-               break;
-               case 2:
-                 Instantiate(teddy2, new Vector2(Random.Range(-10,10),Random.Range(-10,10)), Quaternion.identity);
-               break;
-
-               case 3:
-                 Instantiate(b, new Vector2(Random.Range(-10,10),Random.Range(-10,10)), Quaternion.identity);
-               break;
-
-               case <3:
-                 Instantiate(slime, new Vector2(Random.Range(-10,10),Random.Range(-10,10)), Quaternion.identity);
-               break;
+              Instantiate(c[Random.Range(0,size)], new Vector2(Random.Range(-10,10),Random.Range(-10,10)), Quaternion.identity);
+        
                
           }
           
              
-            }
+     }
 
         
-    }
-}
+  }
+
