04de8b4014eb0a6a6f263923c768cfb4bf622f6e
Tpxrules <tpxrules@gmail.com>
2023-02-14 19:50:49 +0300

    a

--- LeBlood/Assets/codes/lettherebeenemies.cs
+++ LeBlood/Assets/codes/lettherebeenemies.cs
@@ -4,30 +4,23 @@ using UnityEngine;
 
 public class lettherebeenemies : MonoBehaviour
 { 
-     public int size;
-     public float spawntime = 0.1f; 
-     private float timesincelastspawn;
-
-             public GameObject[] c;
+  public int size;
+  public float spawntime = 0.1f; 
+  private float timesincelastspawn;
+  public GameObject[] c;
               
     // Update is called once per frame
-    void Update()
-    {
+  void Update()
+  {  
+    timesincelastspawn += Time.deltaTime;
+    if (timesincelastspawn >= spawntime )
+    { 
+      timesincelastspawn = 0f;
 
-       
-       timesincelastspawn += Time.deltaTime;
-        if (timesincelastspawn >= spawntime )
-        { 
-          timesincelastspawn = 0f;
-
-              Instantiate(c[Random.Range(0,size)], new Vector2(Random.Range(-10,10),Random.Range(-10,10)), Quaternion.identity);
+      Instantiate(c[Random.Range(0,size)], new Vector2(Random.Range(-10,10),Random.Range(-10,10)), Quaternion.identity);
         
                
-          }
-          
-             
-     }
-
-        
-  }
+    }          
+  }      
+}
 
