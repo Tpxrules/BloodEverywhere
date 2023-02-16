c0ba39ca644f05cf706ba0c51e29c51e17aee2a6
Tpxrules <tpxrules@gmail.com>
2023-02-16 17:27:23 +0300

    hot patch for spawning

--- LeBlood/Assets/codes/lettherebeenemies.cs
+++ LeBlood/Assets/codes/lettherebeenemies.cs
@@ -7,12 +7,17 @@ public class lettherebeenemies : MonoBehaviour
   public int range = 20;
   public int size;
   public float spawntime ; 
-  public int spawnamount = 1;
+  public int spawnamount = 2;
+  public int asa  = 1;
   private float timesincelastspawn;
   public GameObject[] c;
               
   public void speedup(){
       spawnamount++;
+
+      if(spawnamount % 2 == 0)
+        asa = spawnamount/2;
+
      if(spawntime  > 0.2)
     
             spawntime = spawntime - 0.1f;   
@@ -24,7 +29,7 @@ public class lettherebeenemies : MonoBehaviour
     if (timesincelastspawn >= spawntime )
     { 
       timesincelastspawn = 0f;
-       for(int i = 0 ; i < spawnamount ; i++)
+       for(int i = 0 ; i < asa ; i++)
       Instantiate(c[Random.Range(0,size)], new Vector2(Random.Range(-range,range),Random.Range(-range,range)), Quaternion.identity);
         
                
