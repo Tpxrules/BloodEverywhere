4274747981a902640494fb455d3ffb5157a1e5f7
Tpxrules <tpxrules@gmail.com>
2023-03-08 17:06:13 +0300

    changed item script name

--- LeBlood/Assets/codes/ItemScript.cs
+++ LeBlood/Assets/codes/ItemScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CritChance : MonoBehaviour
+public class ItemScript : MonoBehaviour
 {
      public GameObject floatingMessagePrefab;
      public int type;
